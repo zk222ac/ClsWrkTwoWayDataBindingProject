a04dbfb6401a833fad6da985139f17b74ee5fe64
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-02 11:41:52 +0100

    Add project files.

---
+++ ClsWrkTwoWayDataBindingProject/Model/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ClsWrkTwoWayDataBindingProject.Annotations;
+
+namespace ClsWrkTwoWayDataBindingProject.Model
+{
+    class Car : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+
+        public Car(int id , string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                 OnPropertyChanged(nameof(ID));
+                
+            }
+         }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+               
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ClsWrkTwoWayDataBindingProject/ViewModel/CarVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClsWrkTwoWayDataBindingProject.Model;
+
+namespace ClsWrkTwoWayDataBindingProject.ViewModel
+{
+    class CarVm
+    {
+        // Define object of your business logic 
+        private Car _car;
+
+        public CarVm()
+        {
+            _car = new Car(12, "Mercedes");
+        }
+
+        // Source Property :
+        public Car Car
+        {
+            get { return _car; }
+            set { _car = value; }
+        }
+
+    }
+}
